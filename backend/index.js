85eb6bfe80747b7d2740feedce2787912235b3ee
Maciej Witkowski <maciej.jan.witkowski@gmail.com>
2021-03-31 23:54:29 +0200

    Create user schema with complete validation, add basic test to check schema definition

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const User = require('./Models/User');
+
 app.get('/', (req, res) => {
   return res.send('Hello World!');
+});
+
+// user schema test
+app.post('/', async (req, res) => {
+  const new_user = new User({
+    ...req.body
+  });
+
+  new_user.save().then(user => res.json(user));
 });
 
 const dbConnData = {
@@ -20,7 +31,8 @@ const dbConnData = {
 mongoose
   .connect(`mongodb+srv://${dbConnData.username}:${dbConnData.password}@clustermain.dxauc.mongodb.net/db?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(response => {
     console.log(`Connected to MongoDB. Database name: "${response.connections[0].name}"`)
