39ef342364d3463947658916ef125595f1831021
AlexKaniewska <72604303+AlexKaniewska@users.noreply.github.com>
2021-04-07 19:53:54 +0200

    Info component and CSS (#31)

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -2,7 +2,91 @@ import React from 'react';
 
 const Info = () => (
     <div>
-        <h1>Info</h1>
+        <h1>Informacje</h1>
+
+        <div className="info-spis">
+            <h2 className="info-title">Czym jest spis?</h2>
+            <p>Spis ludności to – jak podaje Encyklopedia PWN – podstawowe badanie i źródło danych z zakresu statystyki ludności, które ma na celu zebranie informacji o jej stanie i strukturze wg ustalonych cech demograficznych i społeczno-zawodowych, w oznaczonym momencie, na określonym terytorium.</p><br/>
+            <p>Spisy realizowane są zgodnie z zaleceniami i standardami organizacji międzynarodowych, jak UE i ONZ, co umożliwia dokonywanie porównań międzynarodowych. Zgodnie z Art. 4 Rozporządzenia (WE) Nr 763/2008 Parlamentu Europejskiego i Rady z dnia 9.07.2008 r. w sprawie spisów powszechnych ludności i mieszkań, 
+            państwa członkowskie UE zobligowane są do zbierania danych statystycznych. Warto zaznaczyć, że dotacje unijne oraz liczba miejsc w Parlamencie Europejskim przyznawane są m.in. w zależności od liczby mieszkańców danego kraju, opracowanej na podstawie spisów ludności.</p><br/>
+            <p>Spisy powszechne to czas, kiedy państwo, zadając obywatelom kilka pytań, stara się zdiagnozować: „ilu nas jest”, „kim jesteśmy” i „jak żyjemy”. Spisy powszechne obejmują całą populację ludności i mieszkań (w przypadku narodowego spisu powszechnego ludności i mieszkań) 
+            lub gospodarstw rolnych (w przypadku powszechnego spisu rolnego). Oznacza to, że dane uzyskiwane w wyniku spisu powszechnego pochodzą od wszystkich obywateli. Co istotne, w przypadku wielu cech demograficzno-społecznych, jak np. wyznanie, narodowość czy stopień niepełnosprawności, 
+            spisy powszechne są dla państwa jedynym źródłem danych.</p><br/>
+            <p>Uzyskane podczas spisu powszechnego dane indywidualne są opracowywane i przedstawiane w postaci agregatów danych o różnych przekrojach i na różnych poziomach podziału terytorialnego i administracyjnego kraju. Wyniki spisu umożliwiają analizę i ocenę zróżnicowania przemian demograficznych i społecznych w dowolnych przekrojach. 
+            Na ich podstawie rząd podejmuje najważniejsze decyzje gospodarcze i społeczne na kolejne lata.</p>
+        </div>
+        <div className="info-FAQ">
+            <h2 className="info-title">Pytania i odpowiedzi</h2>
+            <h3 className="faq">Jak wziąć udział w spisie?</h3>
+            <div className="info-panel">
+            <p>W związku z rozpoczęciem Narodowego Spisu Powszechnego Ludności i Mieszkań 2021 i jego wydłużeniem do 30 września br. uprzejmie informujemy, że podstawową metodą realizacji obowiązku spisowego jest samospis poprzez stronę internetową.
+            Przypominamy, że internetowo można się spisać również w urzędach gmin i wojewódzkich urzędach statystycznych na specjalnie przygotowanych w tym celu stanowiskach komputerowych przy zachowaniu reżimu sanitarnego.
+            Spisać można się również na infolinii spisowej obsługiwanej przez pracowników statystyki publicznej pod numerem – 22 279 99 99.</p>
+            </div>
+
+            <h3 className="faq">Czy spis jest obowiązkowy?</h3>
+            <div className="info-panel">
+                <p>Po raz pierwszy w historii polskiej statystyki publicznej samospis internetowy będzie obowiązkowy. Oczywiście jeśli ktoś (np. ze względu na zaawansowany wiek, stan zdrowia czy niepełnosprawność) nie będzie się w stanie samospisać, pomogą mu w tym nasi rachmistrzowie spisowi, którzy rozpoczną pracę od 4 maja 2021 r.</p>
+            </div>
+
+            <h3 className="faq">Jak długo będziecie przetwarzać moje dane osobowe?</h3>
+            <div className="info-panel">
+            <p>Zgodnie z art. 32 ust. 1 ustawy o Narodowym Spisie Powszechnym Ludności i Mieszkań dane osobowe zebrane w ramach prac spisowych, mogą być przetwarzane przez okres 100 lat od dnia zakończenia spisu powszechnego.</p>
+            </div>
+
+            <h3 className="faq">Czy ktoś będzie miał wgląd do moich osobistych odpowiedzi?</h3>
+            <div className="info-panel">
+            <p>W procesie przetwarzania danych osobowych uczestniczą wyłącznie osoby upoważnione przez Administratora Danych do przetwarzania danych osobowych z zachowaniem zasady wiedzy koniecznej.
+            Zgodnie z art. 30 ust. 2 ustawy o NSP 2021 przetwarzanie danych osobowych zebranych w ramach prac spisowych nie może być powierzone innemu podmiotowi.</p><br/>
+            <p>GUS nie udostępnia danych osobowych przekazanych w spisie.</p>
+            </div>
+
+            <h3 className="faq">Kto ma dostęp do danych osobowych pozyskanych w czasie spisu?</h3>
+            <div className="info-panel">
+            <p>W procesie przetwarzania danych osobowych uczestniczą wyłącznie osoby upoważnione przez Administratora Danych do przetwarzania danych osobowych z zachowaniem zasady wiedzy koniecznej. 
+                Uwzględniając charakter, zakres, kontekst i cele przetwarzania danych oraz ryzyko naruszenia praw lub wolności osób fizycznych o różnym prawdopodobieństwie i wadze zagrożenia, 
+                Administrator Danych, którym jest Prezes Głównego Urzędu Statystycznego (GUS), wdrożył odpowiednie środki techniczne i organizacyjne, aby przetwarzanie odbywało się zgodnie z 
+                Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO).</p><br/>
+            <p>Wszystkie dane osobowe przetwarzane w ramach prac spisowych są poufne i podlegają szczególnej ochronie, na zasadach określonych w ustawie o statystyce publicznej oraz w ustawie o ochronie danych osobowych. 
+                Dane osobowe od momentu ich zebrania stają się danymi statystycznymi i objęte są tajemnicą statystyczną. Zasada tajemnicy statystycznej ma charakter absolutny, jest wieczysta, obejmuje wszystkie dane osobowe i nie przewiduje żadnych wyjątków dających możliwość jej uchylenia.</p><br/>
+            <p>Osoby wykonujące prace spisowe są obowiązane do przestrzegania tajemnicy statystycznej. Przed przystąpieniem do pracy rachmistrzowie są pouczani o istocie tajemnicy statystycznej i sankcjach za jej niedotrzymanie. 
+                Następnie na ręce właściwego komisarza spisowego składają pisemne przyrzeczenie następującej treści: „Przyrzekam, że będę wykonywać swoje prace na rzecz statystyki publicznej z całą rzetelnością, zgodnie z etyką zawodową statystyka, 
+                a poznane w czasie ich wykonywania dane jednostkowe zachowam w tajemnicy wobec osób trzecich.”</p><br/>
+            <p>Dane pozyskane podczas spisów mogą być wykorzystywane wyłącznie do opracowań, zestawień i analiz statystycznych oraz do aktualizacji operatów do badań statystycznych prowadzonych przez służby statystyki publicznej.
+                Udostępnianie lub wykorzystywanie danych uzyskanych w spisach dla innych niż podane celów jest zabronione, pod rygorem odpowiedzialności karnej.</p>
+            </div>
+
+            <h3 className="faq">Czy zbieranie danych osobowych przez GUS jest zgodne z RODO?</h3>
+            <div className="info-panel">
+            <p>Zbieranie w spisie powszechnym danych osobowych jest obowiązkowe i zgodne z RODO.</p><br/>
+            <p>W związku z organizacją i przeprowadzeniem Narodowego Spisu Powszechnego Ludności i Mieszkań 2021, dane osobowe są przetwarzane zgodnie z art. 6 ust. 1 lit. c RODO – 
+                przetwarzanie jest niezbędne do wypełnienia obowiązku prawnego ciążącego na administratorze w odniesieniu do art. 28 ust. 1 w związku z art. 10 pkt. 1) i 2) i art. 11 pkt. 3) 
+                ustawy z dnia 9 sierpnia 2019 r. o narodowym spisie powszechnym ludności i mieszkań w 2021 r. (zm. Dz.U. z 2020 r. poz. 1486- dalej, jako ustawa o NSP 2021)., art. 6 ust. 1 lit. e RODO – 
+                przetwarzanie jest niezbędne do wykonania zadania realizowanego w interesie publicznym lub w ramach sprawowania władzy publicznej powierzonej administratorowi, art. 9 ust. 2 lit. g RODO – 
+                przetwarzanie jest niezbędne ze względów związanych z ważnym interesem publicznym i art. 9 ust. 2 lit. j RODO – przetwarzanie szczególnych kategorii danych osobowych jest niezbędne do celów statystycznych.</p>
+            </div>
+        </div>
+        <div className="info-row">
+        <div className="info-kontakt">
+            <h2 className="info-h2">Kontakt</h2>
+            <p>Główny Urząd Statystyczny <br/>0-925 Warszawa, Aleja Niepodległości 208
+            <br/> Godziny pracy Urzędu: <br/> poniedziałek - piątek 8.15-16.15</p><br/>
+            <p>REGON: 000331501 <br/> NIP: 5261040828</p><br/>
+            <p>Kancelaria <br/> tel.: +48 22 608 30 00</p>
+            <p>kancelariaogolnaGUS@stat.gov.pl</p>
+        </div>
+
+        <div className="info-statystyczna">
+            <h2 className="info-h2">Infolinia Statystyczna</h2>
+            <p>+48 22 279 99 99 <br/> (opłata zgodna z taryfą operatora)
+            <br/> Konsultanci są dostępni w dni robocze <br/> pon-pt: godz. 8.00-15.00</p>
+        </div>
+
+        <div className="info-informacja">
+            <h2 className="info-h2">Informacja</h2>
+            <p>Godziny pracy Informatorium: <br/> poniedziałek-piątek 8.00-16.00</p>
+        </div>
+        </div>
     </div>
 )
 
