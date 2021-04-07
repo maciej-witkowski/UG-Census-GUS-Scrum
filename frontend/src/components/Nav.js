4827e231ef580c7090a9bac4420d59bbf5bc9055
Jakub Kołosowski <jakubkolosowski@MacBook-Pro-Jakub.local>
2021-04-07 19:19:59 +0200

    navbar and homepage CSS

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import logo from '../logo.png';
+import 'bulma/css/bulma.css';
 
 
 const mapStateToProps = state => ({
@@ -18,27 +19,32 @@ const Nav = ({profile}) => {
 
 
     return (
-        <div className="header">
-            {/*zawsze*/}
-
+        <div className={"navbar box"}>
             <nav>
-                <ul className="nav_links">
-                    <li>
-                        <Link  className="nav_links" to={"/"}>
-                            <img src={logo} alt={"logo-gus"} width={"120px"} height={"50px"}/>
-                        </Link>
-                    </li>
-                    <li><Link  className="nav_links" to={"/stats"}>Statystyki</Link></li>
-                    <li><Link  className="nav_links" to={"/info"}>Informacje</Link></li>
+                <ul className={"navbar-menu"}>
+                    <div className="navbar-brand">
+                        <li className="navbar-start">
+                            <Link className={"navbar-item"} to={"/"}>
+                                <img src={logo} alt={"logo-gus"} width="100"/>
+                                <h6 className={"title has-text-light"} >|</h6>
+                            </Link>
+                        </li>
+                        <li className="navbar-item has-text-centered has-text-weight-bold"><Link  to={"/stats"}>Statystyki</Link></li>
+                        <li className="navbar-item has-text-centered has-text-weight-bold"><Link  to={"/info"}>Informacje</Link></li>
+                    </div>
                 </ul>
             </nav>
-            
-            
 
+            <div className="navbar-end">
+                <div className="navbar-item">
+                        {Object.keys(profile).length === 0? (<button className="button is-light mr-3"><Link className="button is-light" to={"/register"}>Rejestracja</Link></button>):null}
+                        {Object.keys(profile).length === 0? (<button className="button is-info is-1"><Link className="button is-info" to={"/login"}>Logowanie</Link></button>):null}
+                </div>
+            </div>
             {/*przed zalogowaniem*/}
             <div>
-            {Object.keys(profile).length === 0? (<button className="cta"><Link className="cta" to={"/register"}>Rejestracja</Link></button>):null}
-            {Object.keys(profile).length === 0? (<button className="cta"><Link  className="cta" to={"/login"}>Logowanie</Link></button>):null}
+
+
             </div>
 
             {/*po zalogowaniu jako uzytkownik*/}
